85847181506ccf704842907696ccc9fc37c563ef
utamy <ainunsalisutami@gmail.com>
2022-11-11 15:57:34 +0700

    ainun 6

---
+++ application/controllers/admin_harga_patokan/sk.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sk extends MY_OperatorController {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("model_admin_harga_patokan");
+    }
+	public function index()
+	{
+		$data['home_url'] = 'Tampilan_utama';
+		
+        $data["peraturan"] = $this->model_admin_harga_patokan->peraturan();
+        $this->template->load('template', "admin_harga_patokan/sk", $data);
+	}
+	function detail()
+	{		
+			$data["detail"] = $this->model_admin_harga_patokan->detail();
+			$data["rincian"] = $this->model_admin_harga_patokan->_detail();
+
+			$this->template->load('template', 'admin_harga_patokan/detail', $data);
+	}
+}
--- application/views/admin_harga_patokan/sk.php
+++ application/views/admin_harga_patokan/sk.php
@@ -1,1 +1,81 @@
-SK Page
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+
+          <div class="box box-primary">
+            <div class="box-header  with-border">
+              <h3 class="box-title">Data Harga Patokan</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+
+              <table id="mytable" class="table table-striped table-bordered table-hover table-full-width dataTable" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>NOMOR SK</th>
+                        <th>KETERANGAN</th>
+                        <th>FILE</th>
+                        <th>AKSI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $no_urut = 1;
+                    foreach ($peraturan as $sk): ?>
+                    <tr>
+                        <td width="20">
+                            <?php echo $no_urut++; ?>
+                        </td>
+                        <td width="150">
+                            <?php echo $sk->nomor_sk ?>
+                        </td>
+                        <td>
+                            <?php echo $sk->keterangan ?>
+                        </td>
+                        <td width="250">
+                            <a href="<?php echo site_url('harga_patokan/data/detail/'.$sk->id) ?>"
+                                class="btn btn-warning"><i class="fa fa-download"></i> Unduh</a>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+              </table>
+
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+
+<!-- punya lama -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.0/jquery.dataTables.js"></script> -->
+<!-- <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.js"></script> -->
+
+<!-- baru tapi cdn -->
+<!-- <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css"> -->
+
+<script src="<?php echo base_url(); ?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+
+<script>
+        $(document).ready(function() {
+            var t = $('#mytable').DataTable( {
+                "order": [[ 1, 'asc' ]],
+            } );
+               
+            t.on( 'order.dt search.dt', function () {
+                t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                    cell.innerHTML = i+1;
+                } );
+            } ).draw();
+        } );
+</script>
